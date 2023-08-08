f2fe59cc0accd8bb7b77afd0b5ba91ffd27f33ec
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-08-08 11:06:28 -0500

    Agrega factory function para crear players

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ const gameBoard = (() => {
 
     return {updateGameArray,clearGameArray};
 })();
+
+const player = function (name,mark,computer){
+
+    return Object.assign({
+        getName : () => {
+            return name
+        },
+        getMark : () =>{
+            return mark
+        },
+        computer : computer
+    })
+}
+
+const player2 = player("samir","X",false);
+const player1 = player("alonso","0",false)
