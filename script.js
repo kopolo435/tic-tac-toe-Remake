3109918415f862db9a13cbaa8f76b8107a41f055
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-08-09 07:30:21 -0500

    Agrega comentarios

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Modulo encargado de manipular el array que representa el tablero
 const gameBoard = (() => {
     let gameArray = ["","","","","","","","",""];
 
@@ -14,6 +15,7 @@ const gameBoard = (() => {
     return {updateGameArray,clearGameArray,getGameArray};
 })();
 
+//Factory Function para crear los objetos player
 const player = function (name,mark,computer){
 
     return Object.assign({
@@ -27,6 +29,7 @@ const player = function (name,mark,computer){
     })
 }
 
+//Modulo encargado modificar el tablero por medio del dom
 const displayController = (()=>{
     const board = document.getElementById("board");
     const cellsNodes = board.getElementsByClassName("cells");
@@ -52,6 +55,7 @@ const displayController = (()=>{
 const player1 = player("alonso","X",false);
 const player2 = player("samir","0",false);
 
+//Modulo encargado de controlar el juego
 const gameController = ((player1,player2,gameBoard,displayController)=>{
     let currentPlayer = player1;
 
@@ -70,7 +74,7 @@ const gameController = ((player1,player2,gameBoard,displayController)=>{
         ];
         const boardArray = gameBoard.getGameArray();
         for(const combination of winningCombinations){
-            if(boardArray[combination[0]]!== ""){
+            if(boardArray[combination[0]]!== ""){ //Comprueba que la combinacion no este vacia
                 if((boardArray[combination[0]] === boardArray[combination[1]])&&
                 (boardArray[combination[0]] === boardArray[combination[2]])
                 ){
@@ -84,6 +88,7 @@ const gameController = ((player1,player2,gameBoard,displayController)=>{
     return{playRoundPlayer};
 })(player1,player2,gameBoard,displayController);
 
+//Modulo encargado de los events de las cells
 const cellsEventManager = ((gameController)=>{
     const cellsList = document.getElementsByClassName("cells");
 
@@ -91,7 +96,7 @@ const cellsEventManager = ((gameController)=>{
         
         const clickCellHandler = () =>{
             gameController.playRoundPlayer(cell.getAttribute("data-index"));
-            cell.removeEventListener("click", clickCellHandler);
+            cell.removeEventListener("click", clickCellHandler); //Cada evento solo se activa una vez
         };
         cell.addEventListener("click", clickCellHandler);
     };
